06204002744e74df37f77aece67ed1d7abea2c93
Niklas Röttger <niklas.roettger@outlook.de>
2023-11-24 09:48:27 +0100

    add demo code

--- js/index.js
+++ js/index.js
@@ -3,3 +3,22 @@ console.clear();
 const form = document.querySelector('[data-js="card-form"]');
 const textInput = document.querySelector('[data-js="text-input"]');
 const cardContainer = document.querySelector('[data-js="card-container"]');
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // this helps us clear all previous entries
+  cardContainer.innerHTML = "";
+
+  const newCard = document.createElement("li");
+
+  newCard.classList.add("card");
+  const cardText = textInput.value;
+
+  newCard.innerHTML = `
+    <h2 class="card__title">Card</h2>
+    <p>${cardText}</p>
+  `;
+
+  cardContainer.append(newCard);
+});
